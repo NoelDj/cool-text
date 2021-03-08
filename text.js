4c9c01d13b7f672da95223e8a65f1c0a6fbd643e
noeldj <noel0045@stud.kea.dk>
2021-03-08 15:56:16 +0100

    init

---
+++ text.js
@@ -0,0 +1,37 @@
+        document.addEventListener('DOMContentLoaded', () => {
+            const h1 = document.querySelector('h1')
+            const text = h1.textContent
+            let n = 0
+            h1.innerHTML = ''
+
+            let arr = text.split('')
+            let index = -1;
+            let interval = setInterval(function() {
+                arr[index++]
+                const span = document.createElement('span')
+                span.textContent = arr[index];
+
+                console.log( arr[index])
+                if (span.textContent == ' ') {
+
+                    span.style.display = 'inline'
+                    n++;
+                    if(n==3){
+                        n=0
+                        const br = document.createElement('br')
+                    h1.appendChild(br)
+                    } else {
+                        h1.appendChild(span)
+                    }
+
+                } else {
+                    h1.appendChild(span)
+                }
+
+                span.classList.add('move')
+
+                if (index == arr.length) {
+                    clearInterval(interval);
+                }
+            }, 100)
+        })
